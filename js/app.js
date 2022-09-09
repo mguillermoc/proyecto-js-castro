02aa076202e12647c89a47a58d4ebf19affefa18
Martin Castro <mguillermocastro@gmail.com>
2022-09-08 22:57:17 -0300

    First Commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const completo = []
+
+
+function titulo(){
+    pass
+}
+function youtube(){
+    pass
+}
+function souncloud(){
+    pass
+}
+function subtitulo(){
+    pass
+}
+function descarga(){
+    pass
+}
+
+document.getElementById("btn-agregar").addEventListener("click",agregar)
+
+function agregar(){
+    completo.push("1")
+}
+
+console.log(completo)
