b129d474c1072a86904ca285c1d3ff8ba4f593bb
Martin Castro <mguillermocastro@gmail.com>
2022-09-15 23:33:35 -0300

    Eventos

--- js/app.js
+++ js/app.js
@@ -1,26 +1,52 @@
-const completo = []
+let completo = [];
+const BtnAgregar = document.querySelector(".btn")
 
 
-function titulo(){
-    pass
-}
-function youtube(){
-    pass
-}
-function souncloud(){
-    pass
-}
-function subtitulo(){
-    pass
-}
-function descarga(){
-    pass
+BtnAgregar.onclick = function(){
+    const Tipo = document.getElementById("tipo").value
+    const Campo = document.getElementById("seleccion").value
+    completo.push(Campo,Tipo);
+    console.log(completo)
 }
 
-document.getElementById("btn-agregar").addEventListener("click",agregar)
 
-function agregar(){
-    completo.push("1")
-}
 
-console.log(completo)
+
+
+
+
+
+
+
+
+
+
+
+// const test = get.ElementsByClassName("btn");
+
+// test.onclick = function() {
+//     alert("funciona");
+// }
+
+// function titulo(){
+//     pass
+// }
+// function youtube(){
+//     pass
+// }
+// function souncloud(){
+//     pass
+// }
+// function subtitulo(){
+//     pass
+// }
+// function descarga(){
+//     pass
+// }
+
+// document.getElementById("btn-agregar").addEventListener("click",agregar)
+
+// function agregar(){
+//     completo.push("1")
+// }
+
