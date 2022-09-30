cc14808f19cc34c2108808c1674eabaf23730113
Martin Castro <mguillermocastro@gmail.com>
2022-09-29 21:25:43 -0300

    29-09-2022

--- js/app.js
+++ js/app.js
@@ -6,22 +6,6 @@ var BtnBorrar = document.getElementsByClassName("btn2");
 const BtnAgregar = document.querySelector("#agregar");
 const CajaPrincipal = document.querySelector("main")
 
-
- 
-BtnJuntar.onclick = function(){
-    var Formulario = document.getElementsByClassName('formulario');
-    for(var i=0; i< Formulario.length; i++){
-    const inputs = Formulario[i].elements;
-    const Tipo = inputs[0].value;
-    const Campo = inputs[1].value;
-    const objeto = {"Tipo":Tipo,"Campo ID": Campo}
-    completo.push(objeto);
-    console.log(completo);
-}
-}
-
-
-
 BtnAgregar.onclick= function(){
     let FormToDup = document.querySelector(".formulario");
     let clonar = FormToDup.cloneNode(true);
@@ -31,6 +15,49 @@ BtnAgregar.onclick= function(){
     clonar.id = IdUnico += 1
     CajaPrincipal.appendChild(clonar)
 }
+
+BtnJuntar.onclick = function(){
+    Swal.fire({
+        title: '¿Quiere compilar el Formulario?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Si',
+        denyButtonText: 'No',
+        customClass: {
+          actions: 'my-actions',
+          cancelButton: 'order-1 right-gap',
+          confirmButton: 'order-2',
+          denyButton: 'order-3',
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+            var Formulario = document.getElementsByClassName('formulario');
+                 for(var i=0; i< Formulario.length; i++){
+                const inputs = Formulario[i].elements;
+                const Tipo = inputs[0].value;
+                const Campo = inputs[1].value;
+                const objeto = {"Tipo":Tipo,"Campo ID": Campo}
+            }
+        Swal.fire('¡Compilado!', '', 'success')
+    } else if (result.isDenied) {
+  Swal.fire('Cancelado', '', 'info')
+
+}   
+})
+} 
+
+
+// BtnJuntar.onclick = function(){
+//     var Formulario = document.getElementsByClassName('formulario');
+//     for(var i=0; i< Formulario.length; i++){
+//     const inputs = Formulario[i].elements;
+//     const Tipo = inputs[0].value;
+//     const Campo = inputs[1].value;
+//     const objeto = {"Tipo":Tipo,"Campo ID": Campo}
+//     completo.push(objeto);
+//     console.log(completo);
+// }
+// }
 
 
 
